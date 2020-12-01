b6fbb92e65da3cde288073ba95e74f1f6dfbfc23
patriotkusuma <patriot.kusumasejati@gmail.co>
2020-12-02 00:14:29 +0700

    Yoga Pwl

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 //koneksi Database
-$server = "localhost";
-	$user = "root";
-	$pass = "";
-	$database = "dblatihan";
+$server = "us-cdbr-east-02.cleardb.com";
+	$user = "b0fc5f56ad9367";
+	$pass = "a90b0f14";
+	$database = "heroku_4ef18c188c03ee5";
 
 	$koneksi = mysqli_connect($server, $user, $pass, $database)or die(mysqli_error($koneksi));
